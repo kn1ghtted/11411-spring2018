82df728bca89c8c6c43f489a249953ecd3de7d8b
Xingyuan Lin <xingyual@andrew.cmu.edu>
2018-03-21 03:44:30 +0800

    sync

--- qg-exercise/exp.py
+++ qg-exercise/exp.py
@@ -2,6 +2,8 @@
 from stanfordcorenlp import StanfordCoreNLP
 import nltk
 import re
+import json
+import os
 
 # nlp = StanfordCoreNLP('/home/lxy/stanford-corenlp-full-2017-06-09')
 
@@ -14,12 +16,36 @@ import re
 
 # nlp.close() # Do not forget to close! The backend server will consume a lot memery.
 
+
+
+def text2sentences(text):
+    ret = list()
+    # remove non-ascii characters
+    text = re.sub(r'[^\x00-\x7f]',r'', text)
+    for sentence in nltk.sent_tokenize(text):
+        ret += re.split('\n+', sentence)
+    return ret
+
 input_file = "/home/lxy/11611/11411-spring2018/qg-exercise/sample_input.txt"
 with open(input_file, 'r') as f:
     text = f.read()
 
+sentences = text2sentences(text)
+sentences = [x for x in sentences if x[-1:] is '.']
 
-# remove non-ascii characters
-text = re.sub(r'[^\x00-\x7f]',r'', text)
+nlp = StanfordCoreNLP('/home/lxy/stanford-corenlp-full-2017-06-09')
+sentence = 'The Artist is a 2011 French romantic comedy-drama in the style of a black-and-white silent film.'
+tokens = nlp.word_tokenize(sentence)
+pos = nlp.pos_tag(sentence)
+tree = nlp.parse(sentence)
 
-sentences = nltk.sent_tokenize(text)
+props={'annotators': 'pos,parse','pipelineLanguage':'en','outputFormat':'json'}
+tree = nlp.annotate(sentence, properties=props)
+tree = json.loads(tree)
+print json.dumps(tree, sort_keys=True, indent=2, separators=(',', ': '))
+nlp.close()
+
+
+os.environ['STANFORD_PARSER'] = '/home/lxy/stanford-corenlp-full-2017-06-09'
+os.environ['STANFORD_MODELS'] = '/home/lxy/stanford-corenlp-full-2017-06-09'
+parser = StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
