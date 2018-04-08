0a45aefe1acd4b35cd91c7603b7a87015f21d918
kn1ghtted <dingximin@hotmail.com>
2018-04-07 23:06:54 -0400

    added function get_word_supersense()

--- utility/utility.py
+++ utility/utility.py
@@ -1,9 +1,7 @@
-
-
-
 import nltk
 import re
 
+from nltk.corpus import wordnet as wn
 
 def text2sentences(text):
     ret = list()
@@ -12,3 +10,12 @@ def text2sentences(text):
     for sentence in nltk.sent_tokenize(text):
         ret += re.split('\n+', sentence)
     return ret
+
+# get the lexname/supersense of the word
+# E.G.: "student" -> noun.person
+
+def get_word_supersense(w):
+    synsets = wn.synsets(w)
+    if len(synsets) == 0:
+        raise Exception("No supersense found!")
+    return synsets[0].lexname()
