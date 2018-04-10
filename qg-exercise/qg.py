81dd8e0cdaff5236ed2c59a6fe36edb2713f6100
DoudM <kaitlyncel@hotmail.com>
2018-04-10 17:01:50 -0400

    add wh_vp and why questions

--- qg-exercise/qg.py
+++ qg-exercise/qg.py
@@ -7,6 +7,7 @@
 import sys
 sys.path.append('../utility/')
 
+from utility import *
 from stanfordcorenlp import StanfordCoreNLP
 from const_tree import const_tree
 from pattern.en import lemma
@@ -15,6 +16,7 @@ import string
 from nltk.tag.stanford import StanfordNERTagger
 from nltk.tag.stanford import CoreNLPNERTagger
 # from nltk.parse.corenlp import CoreNLPParser
+nltk.download('wordnet')
 
 # parser = CoreNLPParser(url='http://nlp01.lti.cs.cmu.edu:9000/')
 # def _sentenceToConstTree(sentence):
@@ -110,14 +112,13 @@ def generate_whh_question(sentence):
         return None
     # case on VP
     # return analyze_vp_structure(vp, np)
-    return generate_wh_vp_question(vp, np)
+    # return generate_wh_vp_question(vp, np)
+    return generate_why_question(vp, np)
 
 def generate_binary_question(sentence):
     print "[Sentence] ", sentence
     question = ''
     parsed_string = nlp.parse(sentence)
-    # print (nlp.parse('I ate a sandwich.'))
-    # print (nlp.parse('I have eaten a sandwich.'))
     root = const_tree.to_const_tree(parsed_string)
     np = None
     vp = None
@@ -132,10 +133,11 @@ def generate_binary_question(sentence):
     # case on VP
     return analyze_vp_structure(vp, np)
 
+# ask wh question on NP at the VP node.
 def generate_wh_vp_question(node, parent_NP):
     question = ""
     VP = None
-    NP = None # this part to ask!!
+    NP = None # this part to ask!
     VBX = None # for auxiliary verb
     for child in node.children:
         if (child.type == "VP"):
@@ -153,7 +155,7 @@ def generate_wh_vp_question(node, parent_NP):
     # discard sentence with no VBX
     if VBX == None:
         return
-    # ????????????????????? if does not have auxiliary verb and does not have NP
+    # if does not have auxiliary verb and does not have NP
     if NP == None and VP == None: return
 
     # has auxiliary verb
@@ -165,7 +167,7 @@ def generate_wh_vp_question(node, parent_NP):
             if (child.type == "NP"):
                 NP_AUX = child
         VP.children.remove(child)
-        question += "What" + " " # need to modify to ask different Wh questions ?????????????????????????
+        question += "What" + " " # need to modify to ask different Wh questions !!!!!!!!!!!!!!!!!!!!
         question += VBX.to_string()
         question += " " + parent_NP_string + " " + VP.to_string() + "?"
     # no auxiliary verb
@@ -181,13 +183,37 @@ def generate_wh_vp_question(node, parent_NP):
         if vp_without_verb != "":
             vp_without_verb = ' ' + vp_without_verb
 
-        # ask question starting with Wh ??????????????????
+        # ask question starting with Wh !!!!!!!!!!!!!!!!!!!!!
         question += "What" + " "
         if VBX.to_string() in BE_VERBS:
             question += VBX.to_string() + ' ' + parent_NP_string + vp_without_verb + '?'
         else:
             question += VERB_TYPE_AUX_VERB_MAPPING[VBX.type].lower()
             question += ' ' + parent_NP_string + ' ' + lemma(VBX.to_string()) + vp_without_verb + '?'
+    return question
+
+# If there is because in the sentence, generate question asking Why.
+def generate_why_question(node, parent_NP):
+    question = ""
+    SBAR = None
+    for child in node.children:
+        if (child.type == "SBAR"):
+            SBAR = child
+        # Discard sentence with conjunction structure
+        if (child.type == "CC"):
+            return None
+    if (SBAR == None or len(SBAR.children) == 0): return None
+    # find if there is 'because'
+    for child in SBAR.children:
+        if (child.word == "because"):
+            node.children.remove(SBAR)
+            bi_question = analyze_vp_structure(node, parent_NP)
+            # lower case the first letter
+            if len(bi_question) > 0:
+                bi_question = bi_question[0].lower() + bi_question[1:]
+            question += "Why "
+            question += bi_question
+            break
     return question
 
 
@@ -206,7 +232,15 @@ if __name__ == '__main__':
     questions = list()
     # print(generate_binary_question('I ate a sandwich.'))
     # print(generate_binary_question('I have eaten a sandwich.'))
-    print (nlp.parse('I talk to a person.'))
+    print (nlp.parse('I talk to a person because I like talking.'))
+    sentence_test = 'I talk to a person because I like talking.'
+    print ("get question")
+    print generate_whh_question(sentence_test)
+
+    sentence_test2 = "Hazanavicius chose the form of the melodrama, mostly because he thought many of the films from the silent era which have aged best are melodramas."
+    print (nlp.parse(sentence_test2))
+    print "another question"
+    print generate_whh_question(sentence_test2)
 
     # STANFORD_NLP_PATH = "/Users/teddyding/11411/stanford-corenlp-full-2017-06-09"
 
@@ -216,17 +250,18 @@ if __name__ == '__main__':
     # pos = nlp.pos_tag(sentence)
     # tree = nlp.parse(sentence)
     # print nlp.ner(sentence)
-    ner = CoreNLPNERTagger(url='http://nlp01.lti.cs.cmu.edu:9000/')
-    print ner.tag("I talk to a person.".split())
+    # ner = CoreNLPNERTagger(url='http://nlp01.lti.cs.cmu.edu:9000/')
+    # print ner.tag("I talk to a person.".split())
+    # print get_word_supersense("person")
 
-    # for sentence in sentences:
-    #     question1 = generate_binary_question(sentence)
-    #     question2 = generate_whh_question(sentence)
-    #     if (question1 is not None) and (question2 is not None):
-    #         questions.append(question1)
-    #         questions.append(question2)
-    #         print "[Question] ", question1
-    #         print "[Question] ", question2
-            # print '\n'
+    for sentence in sentences:
+        question1 = generate_binary_question(sentence)
+        question2 = generate_whh_question(sentence)
+        # if (question1 is not None) and (question2 is not None):
+        #     questions.append(question1)
+        #     questions.append(question2)
+        #     print "[Question] ", question1
+        #     print "[Question] ", question2
+        #     print '\n'
 
     nlp.close()
