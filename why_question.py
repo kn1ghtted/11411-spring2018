cecaa80a42619658a2691960b3ec26cf9679c3a3
DoudM <kaitlyncel@hotmail.com>
2018-04-24 03:04:29 -0400

    add why question and answer

--- why_question.py
+++ why_question.py
@@ -26,12 +26,44 @@ def generate_why_question(node, parent_NP):
             bi_question = ask_binary_question(node, parent_NP)
             # lower case the first letter
             if len(bi_question) > 0:
-                bi_question = bi_question[0].lower() + bi_question[1:]
+                if len(bi_question) > 1:
+                    bi_question = bi_question[0].lower() + bi_question[1:]
+                else:
+                    bi_question = bi_question[0].lower()
             question += "Why "
             question += bi_question
             break
     return question
 
 
-def answer_why(Q):
-    return None
+def answer_why(Q, most_relevant_sentence, node, parent_NP):
+    answer = most_relevant_sentence
+    SBAR = None
+    for child in node.children:
+        if (child.type == "SBAR"):
+            SBAR = child
+        # Discard sentence with conjunction structure
+        if (child.type == "CC"):
+            return answer
+    if (SBAR == None or len(SBAR.children) == 0): return answer
+    # find if there is 'because'
+    del_child = []
+    for child in SBAR.children:
+        if (child.word == "because"):
+            # Answer with because
+            for c in del_child:
+                SBAR.children.remove(c)
+            answer = SBAR.to_string()
+            if (len(answer) > 0):
+                if (len(answer) > 1):
+                    answer = answer[0].upper() + answer[1:]
+                else:
+                    answer = answer[0].upper()
+            else:
+                return answer
+            answer += "."
+            break
+        else:
+            del_child.append(child)
+    return answer
+
