1b8f25dd1040e68095c91bc7d65eac4812ba1c56
Sphinx and Oedipus <sphinxoedipus@nlp.lti.cs.cmu.edu>
2018-04-26 19:41:17 -0400

    change server url

--- statics.py
+++ statics.py
@@ -10,10 +10,10 @@ VERB_TYPE_AUX_VERB_MAPPING = {'VB': 'Do', 'VBZ': 'Does', 'VBP': 'Do', 'VBD': 'Di
 from nltk.tag.stanford import CoreNLPNERTagger
 URL1 = "http://nlp01.lti.cs.cmu.edu:9000/"
 URL_LOCAL = "http://localhost:11411/"
-tagger = CoreNLPNERTagger(url=URL_LOCAL)
+tagger = CoreNLPNERTagger(url=URL1)
 
 from nltk.parse.corenlp import CoreNLPParser
-parser = CoreNLPParser(url=URL_LOCAL)
+parser = CoreNLPParser(url=URL1)
 
 import sys
 sys.path.append("./utility/")
